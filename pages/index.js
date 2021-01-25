7070b7a66d36b388c0b23547bfb0bde5d6276dee
franciellefmorais@hotmail.com <franciellefmorais@hotmail.com>
2021-01-25 17:41:50 -0300

    código ao final da primeira aula da imersão React

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,57 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/Widget'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+export const QuizContainer = styled.div`
+width: 100%;
+max-width: 350px;
+padding-top: 45px;
+margin: auto 10%;
+@media screen and (max-width: 500px) {
+margin: auto;
+padding: 15px;
+}
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+
+            <Widget>
+              <Widget.Header>
+                <h1>Info Quiz</h1>
+              </Widget.Header>
+
+              <Widget.Content>
+                <p>Teste os seus conhecimentos sobre informática básica e vamos ver em qual nível você está!</p>
+              </Widget.Content>
+            </Widget>
+
+            <Widget>
+              <Widget.Header>
+                <h1>Quizes da Galera</h1>
+              </Widget.Header>
+
+              <Widget.Content>
+                <p>lorem lorem lorem</p>
+              </Widget.Content>
+            </Widget>
+
+          <Footer />
+        </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/imfranmorais" />
+    </QuizBackground>
+  );
 }
